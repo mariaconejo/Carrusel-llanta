16ac15f4e215c6d1ddef8486d99369b37924b6a0
mariaconejo <mary03081@gmail.com>
2025-07-03 20:13:45 -0600

    se agrego base en vite config

--- carrusel-llanta-nuevo/vite.config.ts
+++ carrusel-llanta-nuevo/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Carrusel-llanta/',
+  base: '/Carrusel-llanta/',    
   plugins: [react()],
 })
